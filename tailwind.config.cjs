b0ace087815f675526172f93c9e426aff4c0daf2
William Grigor <bjgrigor@gmail.com>
2022-07-25 21:33:53 -0500

    Added Home page Making progress on #2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,20 @@
+const heroColors = {
+  'hero-red': '#ff0000',
+  'hero-yellow': '#f5f500',
+  'hero-blue': '#0000dc' 
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: heroColors,
+      borderColor: heroColors,
+      ringColor: heroColors
+    },
   },
   plugins: [],
 }
