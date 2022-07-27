ee8db615fc252c2856ed9b083018b153f4c5c9b4
William Grigor <bjgrigor@gmail.com>
2022-07-26 21:00:42 -0500

    Added alias for components folder

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@pages': resolve(__dirname, 'src', 'pages'),
+      '@components': resolve(__dirname, 'src', 'components'),
       '@assets': resolve(__dirname, 'src', 'assets')
     }
   }
