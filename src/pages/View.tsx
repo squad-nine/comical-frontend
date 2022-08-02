6fc293f78187a9fdf9186e28ea80f458e8dea6e8
William Grigor <bjgrigor@gmail.com>
2022-08-01 20:47:49 -0500

    Added fetching for View route

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useToggle } from 'react-use'
+import { useParams } from 'react-router-dom'
 import { Formik, Form, Field } from 'formik'
 import TextField from '@components/Fields/Text'
+import axios from 'axios'
 
 type ViewState = {
     image: string,
@@ -9,20 +11,37 @@ type ViewState = {
     issueNum: number
 }
 
+type ViewRouteParams = {
+    id: string
+}
+
 const View = () => {
+
+    const { id } = useParams<ViewRouteParams>()
+
     const [ state, setState ] = useState<ViewState>({
-        image: "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/18-1433-17-1-spy-cases.jpg",
-        issueNum: 13,
-        name: "Spy Cases"
+        image: "",
+        issueNum: 0,
+        name: ""
     })
 
+    useEffect(() => {
+        axios.get<ViewState>(`/api/comics/${id}`)
+            .then(({ data }) => {
+                setState(data)
+                setLoading(false)
+            })
+    }, [])
+
     const [ editing, toggleEditing ] = useToggle(false)
+    const [ loading, setLoading ] = useState(true)
 
     return (
         <div className="h-full flex items-center justify-center bg-blue-300">
             <div className="flex flex-col md:flex-row gap-2 bg-dots-pattern bg-dots-color shadow-hard-border border-black p-3">
                 <img src={state.image} alt={state.name} />
-                <Formik initialValues={state} onSubmit={console.log}>
+                { !loading && (
+                    <Formik initialValues={state} onSubmit={console.log}>
                     {() => (
                         <Form className="font-bangers text-white text-3xl">
                             { editing ? (
@@ -41,7 +60,8 @@ const View = () => {
                             <button onClick={toggleEditing} className="w-full hover:opacity-90 bg-hero-red ring-4 ring-inset ring-hero-yellow border-2 border-hero-red py-3 px-10 lg:py-7 lg:px-20 rounded-full text-white text-lg md:text-2xl f-f-p transition-opacity">Edit</button>
                         </Form>
                     )}
-                </Formik>
+                    </Formik>)
+                }
             </div>
         </div>
     )
