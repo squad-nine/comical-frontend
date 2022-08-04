6e73bb72d8e71ac0edf6c3c4b9db08bf178cd581
Urvil Patel <urvilpatel@Urvils-MacBook-Pro.local>
2022-08-03 22:58:00 -0400

    fixed indent

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -93,8 +93,8 @@ const View = () => {
                             <DeleteButton docId={id!} />
                             <>
           
-          <button onClick={()=>navigate("/comics")} className="w-full hover:opacity-90 bg-hero-red ring-4 ring-inset ring-hero-yellow border-2 border-hero-red py-3 px-10 lg:py-7 lg:px-20 rounded-full text-white text-lg md:text-2xl f-f-p transition-opacity">Back</button>
-        </>
+                            <button onClick={()=>navigate("/comics")} className="w-full hover:opacity-90 bg-hero-red ring-4 ring-inset ring-hero-yellow border-2 border-hero-red py-3 px-10 lg:py-7 lg:px-20 rounded-full text-white text-lg md:text-2xl f-f-p transition-opacity">Back</button>
+                            </>
                         </Form>
                     )}
                     </Formik>)
