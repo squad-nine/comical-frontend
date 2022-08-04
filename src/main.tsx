e02874b130beb2bc352a634575aac185983891e3
William Grigor <bjgrigor@gmail.com>
2022-08-03 19:03:48 -0500

    Prepare for deployment with Heroku

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 import './index.css'
 
-axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
