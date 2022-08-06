bcad90779d31ff5c16156c8d20e8c6733d2649e8
William Grigor <bjgrigor@gmail.com>
2022-08-05 19:32:39 -0500

    Login submit button greys out when invalid

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -41,7 +41,7 @@ const Login = () => {
                             setSubmitting(false)
                         })
                 }}>
-                    {({ isSubmitting, status }) => (
+                    {({ isSubmitting, status, isValid }) => (
                         <Form className="text-white border-8 border-black shadow-hard-border bg-dots-pattern bg-dots-color lg:w-1/3  md:w-1/2 w-full p-10 mt-16 flex flex-col gap-6">
                             <p tabIndex={0} role="heading" aria-label="Login to your account" className="text-outline-black text-2xl font-extrabold leading-6">Sign In</p>
                             <p className="text-sm mt-4 font-medium leading-none stroke-black">
@@ -58,7 +58,7 @@ const Login = () => {
                                 <div className="border-2 border-hero-yellow p-2 bg-black bg-opacity-50">{status.message}</div>
                             ) : null }
                             <div className="mt-8">
-                                <button type="submit" role="button" aria-label="create my account" className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full disabled:cursor-wait disabled:bg-indigo-400" disabled={isSubmitting}>Sign in</button>
+                                <button type="submit" role="button" aria-label="create my account" className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full disabled:cursor-wait disabled:bg-indigo-400" disabled={isSubmitting || !isValid }>Sign in</button>
                             </div>
                         </Form>
                     )}
