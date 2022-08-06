92ce189204084f81002a8212ed6bf52689cd8e41
William Grigor <bjgrigor@gmail.com>
2022-08-05 20:17:15 -0500

    Added validation to new comic

--- src/components/Fields/Text.tsx
+++ src/components/Fields/Text.tsx
@@ -1,12 +1,15 @@
 import type { FC } from 'react'
 import { useField, type FieldAttributes } from 'formik'
+import ErrorMessage from '@components/ErrorMessage'
 
 const CustomTextInput: FC<FieldAttributes<string>> = props => {
     const [ field, meta ] = useField<string>(props)
+    const errorMessage = meta.error && meta.touched ? meta.error : undefined
     return (
         <div>
             <label className="text-outline-black" htmlFor={props.name}>{props.placeholder || props.name.at(0)?.toUpperCase() + props.name.slice(1)}</label>
             <input className="form-input bg-gray-200 border border-black text-xs font-medium leading-none w-full mt-2 text-black" type={props.type} id={props.name} {...field} />
+            <ErrorMessage message={errorMessage} />
         </div>
     )
 }
