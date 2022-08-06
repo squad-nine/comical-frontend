63400ddeb49ca1350de6641060e857173996b193
William Grigor <bjgrigor@gmail.com>
2022-08-05 19:04:00 -0500

    Added errors to fields in preparation for validation

--- src/components/Fields/Password.tsx
+++ src/components/Fields/Password.tsx
@@ -3,6 +3,7 @@ import { useToggle } from 'react-use'
 import { useField, type FieldAttributes } from 'formik'
 
 import { EyeSolid as EyeIcon, EyeOffSolid as EyeOffIcon } from '@graywolfai/react-heroicons'
+import ErrorMessage from '@components/ErrorMessage'
 
 type PasswordFieldProps = FieldAttributes<string> & {
     confirm: boolean
@@ -14,10 +15,11 @@ const PasswordField: FC<PasswordFieldProps> = props => {
     const [ passwordVisibility, togglePasswordVisibility ] = useToggle(false)
 
     const [ { name, ...field }, meta ] = useField(props)
+    const errorMessage = meta.error && meta.touched ? meta.error : undefined
     return (
     <div className="mt-6 w-full">
-        <label htmlFor={name} className="text-sm font-medium leading-none">{ confirm ? 'Confirm' : null} Password</label>
-        <div className="relative flex items-center justify-center">
+        <label htmlFor={name} className="text-sm font-medium leading-none">{ confirm ? 'Confirm' : null } Password</label>
+        <div className="relative grid place-items-center">
             <input id={name} aria-label="enter Password" role="input" type={ passwordVisibility ? 'text' : 'password' } className="text-black bg-gray-200 border rounded focus:outline-none text-xs font-medium leading-none py-3 w-full pl-3 mt-2" {...field} />
             <div className="absolute right-0 mt-2 mr-3 cursor-pointer">
                 { passwordVisibility ? (
@@ -27,6 +29,7 @@ const PasswordField: FC<PasswordFieldProps> = props => {
                 )}
             </div>
         </div>
+        <ErrorMessage message={errorMessage} />
     </div>
     )
 }
